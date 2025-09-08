b0aaa678fa6d8f8480a6407d185acff2bae0907c
Tianrider <hadiwijayachristian7@gmail.com>
2025-09-08 20:11:39 +0700

    Refactor divisionIcons for Consistency and Readability
    - Rearranged the order of icon objects in the divisionIcons array for improved consistency.
    - Cleaned up code formatting to enhance readability.

--- src/components/divisions/divisionsIcons.tsx
+++ src/components/divisions/divisionsIcons.tsx
@@ -1,26 +1,25 @@
 export const divisionIcons: Array<{
-	src: string;
-	selectedSrc: string;
-	alt: string;
-	id: string;
+    src: string;
+    selectedSrc: string;
+    alt: string;
+    id: string;
 }> = [
-
-	{
-		src: '/div_logo/palette.svg',
-		selectedSrc: '/div_logo/selected/palette.svg',
-		alt: "Palette Icon",
-		id: "palette",
-	},
-	{
-		src: '/div_logo/laptop.svg',
-		selectedSrc: '/div_logo/selected/laptop.svg',
-		alt: "Laptop Icon",
-		id: "laptop",
-	},
-	{
-		src: '/div_logo/cpu.svg',
-		selectedSrc: '/div_logo/selected/cpu.svg',
-		alt: "CPU Icon",
-		id: "cpu",
-	},
+    {
+        src: "/div_logo/laptop.svg",
+        selectedSrc: "/div_logo/selected/laptop.svg",
+        alt: "Laptop Icon",
+        id: "laptop",
+    },
+    {
+        src: "/div_logo/cpu.svg",
+        selectedSrc: "/div_logo/selected/cpu.svg",
+        alt: "CPU Icon",
+        id: "cpu",
+    },
+    {
+        src: "/div_logo/palette.svg",
+        selectedSrc: "/div_logo/selected/palette.svg",
+        alt: "Palette Icon",
+        id: "palette",
+    },
 ] as const;
